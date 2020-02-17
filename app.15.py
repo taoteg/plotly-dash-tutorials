7313c98e78b45de88f893a6702856b91155d67d0
John Gentle <taoteg@gmail.com>
2020-02-17 14:08:33 -0600

    Added examples of using prevent update in callbacks.

---
+++ app.15.py
@@ -0,0 +1,35 @@
+import dash
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+"""
+Using PreventUpdate in Callback
+
+In certain situations, you don't want to update the callback output.
+You can achieve this by raising a PreventUpdate exception in the callback function.
+"""
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Button('Click here to see the content', id='show-secret'),
+    html.Div(id='body-div')
+])
+
+
+@app.callback(
+    Output(component_id='body-div', component_property='children'),
+    [Input(component_id='show-secret', component_property='n_clicks')]
+)
+def update_output(n_clicks):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        return "Elephants are the only animal that can't jump"
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
