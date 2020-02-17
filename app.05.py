b1045956e626f0a07df163edcca595f84aa88bd5
John Gentle <taoteg@gmail.com>
2020-02-17 12:50:42 -0600

    Added example of rendering markdown using dash.

---
+++ app.05.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+"""
+While Dash exposes HTML through the dash_html_components library, it can be
+tedious to write your copy in HTML. For writing blocks of text, you can use the
+Markdown component in the dash_core_components library.
+
+Dash apps can be written in Markdown.
+Dash uses the CommonMark specification of Markdown.
+Check out their 60 Second Markdown Tutorial if this is your first introduction to Markdown!
+
+CommonMark: http://commonmark.org/
+Tutorial: http://commonmark.org/help/
+"""
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+app.layout = html.Div([
+    dcc.Markdown(children=markdown_text)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
