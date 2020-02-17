506806d1ae75b5891a7cf51fc12b4963ed63ad39
John Gentle <taoteg@gmail.com>
2020-02-17 12:53:04 -0600

    Added example of available components in the dash library.

---
+++ app.06.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+"""
+The dash_core_components includes a set of higher-level components like
+dropdowns, graphs, markdown blocks, and more.
+
+Like all Dash components, they are described entirely declaratively.
+Every option that is configurable is available as a keyword argument of the component.
+
+We'll see many of these components throughout the tutorial.
+You can view all of the available components in the Dash Core Components Gallery:
+https://dash.plot.ly/dash-core-components
+
+Here are a few of the available components.
+"""
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
+
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF']
+    ),
+
+    html.Label('Text Input'),
+    dcc.Input(value='MTL', type='text'),
+
+    html.Label('Slider'),
+    dcc.Slider(
+        min=0,
+        max=9,
+        marks={i: 'Label {}'.format(i) if i == 1 else str(i)
+               for i in range(1, 6)},
+        value=5,
+    ),
+], style={'columnCount': 2})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
